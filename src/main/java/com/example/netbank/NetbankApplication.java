62d6456634a886896fa4f05b2c1e77a22f15cfcf
hr_gergo <gergohrabina@gmail.com>
2025-04-03 19:43:45 +0200

    19:43

---
+++ src/main/java/com/example/netbank/NetbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.netbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbankApplication.class, args);
+	}
+
+}
